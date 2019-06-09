aa8ca596031ed9c6a4da747e4b1dcfb8c59da809
rashi53 <51444233+rashi53@users.noreply.github.com>
2019-06-09 15:20:56 +0530

    Add files via upload

---
+++ volbox.py
@@ -0,0 +1,6 @@
+import math
+l=float(input("enter length"))
+b=float(input("enter breadth"))
+h=float(input("enter height"))
+vol=l*b*h
+print("the volume is ",vol)
---
+++ volearth.py
@@ -0,0 +1,5 @@
+import math
+p=math.pi
+r=6.4*10**6
+vol=4/3*p*r**3
+print("the volume of earth is metre cube is:",vol)
